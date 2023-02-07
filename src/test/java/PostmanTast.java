0d12f471a15aa50d956f5f505d7c0e1f49f20ca0
Vladimir Semenov <osmyswait@gmail.com>
2023-02-07 11:28:36 +0300

    drop test

--- src/test/java/PostmanTast.java
+++ src/test/java/PostmanTast.java
@@ -16,7 +16,7 @@ class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some date"))
         ;
     }
 }
